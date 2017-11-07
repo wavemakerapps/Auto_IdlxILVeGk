b6bf21b0f967f0908eebe4b802ec501538b90b98
apptest <appTest1@wavemaker.com>
2017-11-07 21:14:57 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_IdlxILVeGk",
+  "homePage" : "Main",
+  "name" : "Auto_IdlxILVeGk",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
